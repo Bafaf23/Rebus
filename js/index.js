f0b6d9d624067db62854e446f4f105be87f9d656
Bryant Facenda <bryantffacen@gmail.com>
2025-12-12 21:59:20 -0400

    cambio de la clase conten y estilos generales
    😶‍🌫️

--- js/index.js
+++ js/index.js
@@ -1,36 +1,43 @@
-function guardarUser(){
+function guardarUser() {
+  const namem = "Bryant Facenda";
 
-    const namem = "Bryant Facenda"
-
-    document.getElementById("user").textContent = `¡Hola!, ${namem}`
+  document.getElementById("user").textcontent = `¡Hola!, ${namem}`;
 }
 
 // Guardar la fecha y hora de la última conexión
 function guardarUltimaConexion() {
-  localStorage.setItem('ultimaConexion', new Date().toISOString());
+  localStorage.setItem("ultimaConexion", new Date().toISOString());
 }
 
 // Mostrar la última conexión
 function mostrarUltimaConexion() {
-  const ultimaConexion = localStorage.getItem('ultimaConexion');
+  const ultimaConexion = localStorage.getItem("ultimaConexion");
   if (ultimaConexion) {
-
     const fecha = new Date(ultimaConexion);
 
-    const opciones = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const opciones = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
 
-    const fechaFormateada = fecha.toLocaleDateString('es-ES', opciones);
+    const fechaFormateada = fecha.toLocaleDateString("es-ES", opciones);
 
-    document.getElementById('ultimoAcceso').textContent = `Última conexión: ${fechaFormateada}`;
-
+    document.getElementById(
+      "ultimoAcceso"
+    ).textcontent = `Última conexión: ${fechaFormateada}`;
   } else {
-    document.getElementById('ultimoAcceso').textContent = 'Nunca te has conectado.';
+    document.getElementById("ultimoAcceso").textcontent =
+      "Nunca te has conectado.";
   }
 }
 
 // Llamar a la función al cargar la página
-window.onload = function() {
+window.onload = function () {
   guardarUltimaConexion();
   mostrarUltimaConexion();
-  guardarUser()
+  guardarUser();
 };
